bb5c117ec653715cc83933fc082b2e340740c08a
r3io4mf <73582245+r3io4mf@users.noreply.github.com>
2024-06-19 17:42:31 +0800

    Add AskQuestions.com to Github

---
+++ Combining it all.py
@@ -0,0 +1,16 @@
+# a python file inside pages 
+import pandas as pd
+from PIL import Image
+import io
+import os
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+import uuid
+# main_page
+
+st.set_page_config(page_title = 'Learning Hub!',page_icon = ':school:')
+st.sidebar.markdown(" Main Page🎈")
+st.title('Hey There! Welcome to Learning Hub! Sign in now and ask your question!')
+st.markdown('Forum Page 🖊️')
+st.sidebar.markdown('Forum Page 🖊️')
